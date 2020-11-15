dfc0c289369e765373e90e25071c59c16459dc93
David Zweili <david.zweili@gmail.com>
2020-11-15 12:36:32 -0800

    Add CI configuration

---
+++ Assets/Editor/WebGLBuilder.cs
@@ -0,0 +1,13 @@
+﻿using UnityEditor;
+
+class WebGLBuilder
+{
+    static void build()
+    {
+        string[] scenes = {"Assets/Scenes/MainScene.unity"};
+
+        string pathToDeploy = "builds/WebGL/";
+
+        BuildPipeline.BuildPlayer(scenes, pathToDeploy, BuildTarget.WebGL, BuildOptions.None);
+    }
+}
