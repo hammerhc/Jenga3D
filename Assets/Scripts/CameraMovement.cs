5cd3652c235bdce6ef997f00bfe8bd94251cc448
hammerhc <kriegersimon4@gmail.com>
2020-11-19 09:31:34 +0100

    Camera Drag added

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public GameObject m_Ground;
+    public float dragSpeed = 2;
+
+    private Vector3 lastDragPosition;
+
+    private void LateUpdate()
+    {
+        Camera.main.transform.LookAt(m_Ground.transform.position);
+
+        if (Input.GetMouseButtonDown(0))
+            lastDragPosition = Input.mousePosition;
+        if (Input.GetMouseButton(0))
+        {
+            var delta = lastDragPosition - Input.mousePosition;
+            transform.Translate(delta * Time.deltaTime * dragSpeed);
+            lastDragPosition = Input.mousePosition;
+        }
+    }
+}
