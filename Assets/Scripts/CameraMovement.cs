4bddfe446a7db9be0d26fd67232db898d3ed70c6
hammerhc <kriegersimon4@gmail.com>
2020-11-26 09:36:21 +0100

    Updated Drag Cameramovement

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -2,25 +2,50 @@
 
 public class CameraMovement : MonoBehaviour
 {
-    public GameObject m_Ground;
-    public float dragSpeed = 2;
+    public Transform target;
+    public float dragSpeed;
+    public float zoomSpeed;
 
     private Vector3 lastDragPosition;
-    
+    private float minDistance = 15f;
+    private float maxDistance = 90f;
+    private float distance = 50f;
+    private float rotationYAxis = 0.0f;
+    private float rotationXAxis = 0.0f;
+
     void Update()
+    {
+        scrollCamera();
+        moveCamera();
+    }
+
+    void LateUpdate()
+    {
+        transform.LookAt(target.position);
+    }
+
+    void moveCamera()
     {
         if (Input.GetMouseButtonDown(0))
             lastDragPosition = Input.mousePosition;
         if (Input.GetMouseButton(0))
         {
-            var delta = lastDragPosition - Input.mousePosition;
-            transform.Translate(delta * Time.deltaTime * dragSpeed);
+            Vector3 delta = lastDragPosition - Input.mousePosition;
+
+            rotationYAxis = delta.x * dragSpeed * Time.deltaTime;
+            rotationXAxis = delta.y * dragSpeed * Time.deltaTime;
+
+            transform.RotateAround(target.position, transform.up, rotationYAxis);
+            transform.RotateAround(target.position, transform.right, rotationXAxis);
+
             lastDragPosition = Input.mousePosition;
         }
     }
 
-    void LateUpdate()
+    void scrollCamera()
     {
-        Camera.main.transform.LookAt(m_Ground.transform.position);
+        distance += Input.GetAxis("Mouse ScrollWheel") * zoomSpeed * Time.deltaTime;
+        distance = Mathf.Clamp(distance, minDistance, maxDistance);
+        transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, distance);
     }
 }
