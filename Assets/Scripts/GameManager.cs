8204f5dd84739f527284da7a581495559f257a5b
hammerhc <hammerhc@users.noreply.github.com>
2020-11-28 10:59:38 +0100

    Added Jenga Tower
    Added Gamemanager
    Auto Build Tower with Script

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject towerRow;
+
+    void Start()
+    {
+        float height = 1f;
+        for (int i = 0; i < 26; i++)
+        {
+            if (i % 2 == 1)
+            {
+                Instantiate(towerRow, new Vector3(0, height, 0), Quaternion.identity);
+            }
+            else
+            {
+                Instantiate(towerRow, new Vector3(0, height, 0), Quaternion.Euler(new Vector3(0f, 90f, 0f)));
+            }
+            height += 1.5f;
+        }
+    }
+}
