9545c6bae383bf4fd9c78ce82a77b8fc0be545c1
hammerhc <kriegersimon4@gmail.com>
2020-12-03 09:24:48 +0100

    Adding Shooting functionality

---
+++ Assets/Scripts/Shoot.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    public GameObject bullet;
+    public float bulletSpeed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            GameObject bulletInstance = Instantiate(bullet, transform.position, Quaternion.identity) as GameObject;
+            bulletInstance.GetComponent<Rigidbody>().AddForce(transform.up * bulletSpeed);
+        }
+    }
+}
