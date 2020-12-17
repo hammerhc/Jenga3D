6a91d851b0ac16d7827033bee35b1f68882939b5
slashercash <david.zweili@gmail.com>
2020-12-17 08:04:33 +0100

    improved pause screen

--- Assets/Scripts/UI/UIController.cs
+++ Assets/Scripts/UI/UIController.cs
@@ -5,42 +5,56 @@ using UnityEngine.UIElements;
 public class UIController : MonoBehaviour
 {
     VisualElement elementPause;
+    VisualElement elementControls;
 
     void OnEnable()
     {
         var rootVisualElement = GetComponent<UIDocument>().rootVisualElement;
 
         elementPause = rootVisualElement.Q("ElementPause");
+        elementControls = rootVisualElement.Q("ElementControls");
+        var buttonResume = elementPause.Q<Button>("ButtonResume");
+        var buttonControls = elementPause.Q<Button>("ButtonControls");
+        var buttonExit = elementPause.Q<Button>("ButtonExit");
+        var buttonBack = elementControls.Q<Button>("ButtonBack");
 
-        var resumeButton = elementPause.Q<Button>("ButtonResume");
-        resumeButton.RegisterCallback<ClickEvent>(ev => DisablePauseElement());
-        
+        buttonResume.RegisterCallback<ClickEvent>(ev => EnablePauseElement(false));
+        buttonControls.RegisterCallback<ClickEvent>(ev => SwitchToControls());
+        buttonBack.RegisterCallback<ClickEvent>(ev => SwitchToPause());
+        buttonExit.RegisterCallback<ClickEvent>(ev => ExitGame());
+
     }
 
     void Update()
     {
         if (Input.GetButtonDown("Pause"))
         {
-            EnablePauseElement();
+            EnablePauseElement(true);
         }
     }
 
 
-    void EnablePauseElement()
+    void EnablePauseElement(bool enabled)
     {
-        elementPause.style.display = DisplayStyle.Flex;
-        EnableMouse(true);
-    }
-
-    void DisablePauseElement()
-    {
-        elementPause.style.display = DisplayStyle.None;
-        EnableMouse(false);
-    }
-
-    void EnableMouse(bool enabled)
-    {
+        elementPause.style.display = enabled ? DisplayStyle.Flex : DisplayStyle.None;
         UnityEngine.Cursor.visible = enabled;
         UnityEngine.Cursor.lockState = enabled ? CursorLockMode.None : CursorLockMode.Locked;
+    }
+
+    void SwitchToControls()
+    {
+        elementPause.style.display = DisplayStyle.None;
+        elementControls.style.display = DisplayStyle.Flex;
+
+    }
+    void SwitchToPause()
+    {
+        elementPause.style.display = DisplayStyle.Flex;
+        elementControls.style.display = DisplayStyle.None;
+    }
+
+    void ExitGame()
+    {
+        Application.Quit();
     }
 }
