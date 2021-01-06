b87651971d0b601a6a2889a9c835c5fb1ec32a2d
hammerhc <kriegersimon4@gmail.com>
2021-01-06 10:58:08 +0100

    Added Shot Effect

--- Assets/Scripts/Shoot.cs
+++ Assets/Scripts/Shoot.cs
@@ -4,6 +4,7 @@ public class Shoot : MonoBehaviour
 {
     public float bulletSpeed;
     public float fireRate;
+    public ParticleSystem laserShot;
 
     private float nextTimeToFire = 0f;
 
@@ -13,10 +14,10 @@ public class Shoot : MonoBehaviour
         if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire && Time.timeScale == 1)
         {
             SoundManagerScript.PlaySound("laserShot");
+            laserShot.Play();
 
             nextTimeToFire = Time.time + 1f / fireRate;
-            RaycastHit hit;
-            if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, 100))
+            if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out RaycastHit hit, 100))
             {
                 if (hit.rigidbody != null)
                 {
